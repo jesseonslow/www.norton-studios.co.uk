05fd2d0235276504fd5e36702d66d5dbb59e4909
Linus Norton <linusnorton@gmail.com>
2025-02-20 20:18:21 +0000

    Set up next.js app

---
+++ src/app/page.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/logo.png"
+          alt="Norton Studios Logo"
+          width={180}
+          height={38}
+          priority
+        />
+
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <h1 className="text-4xl font-bold text-center sm:text-left">Norton Studios Ltd</h1>
+          <p className="text-lg text-center sm:text-left">Public Sector Digital Services</p>
+
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="/case-studies"
+        >
+          Case Studios
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="/about-us"
+        >
+          About Us
+        </a>
+      </footer>
+    </div>
+  );
+}
