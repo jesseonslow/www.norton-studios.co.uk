9a30574f4798bc9cec8ba612a1a9e429f8607b23
Perry Harlock <perry@hussle.com>
2025-02-22 15:34:47 +0000

    add footer and header

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,21 +2,9 @@ import { Container } from "../components/Container"
 
 export const Footer = () => {
   return (
-    <footer className="py-20">
+    <footer className="py-6 border-t-2 border-t-teal-500">
       <Container>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="/case-studies"
-        >
-          Case Studios
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="/about-us"
-        >
-          About Us
-        </a>
-        <p>&copy; {new Date().getFullYear()} Norton Studios</p>
+        <p className="text-center md:text-right">&copy; {new Date().getFullYear()} Norton Studios</p>
       </Container>
     </footer>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
-import {Container} from "../components/Container"
+import { twMerge } from "tailwind-merge";
+import {Container} from "../components/Container";
 
 export const Header = () => {
+  const linkClasses = "flex uppercase hover:underline hover:underline-offset-4 p-2 md:px-4 md:py-3 bg-teal-400 font-bold hover:bg-teal-200"
+  const mobileLinkClasses = "px-2 py-4 hover:underline hover:underline-offset-4"
+
   return (
-      <header className="pt-4">
-        <Container className="flex justify-between">
+      <header className="py-4 border-b-2 border-b-teal-500">
+        <Container className="flex justify-between align-middle items-center relative">
           <div>
             {/* <Image
               className="dark:invert"
@@ -31,15 +35,39 @@ export const Header = () => {
               priority
             /> */}
           </div>
-          <nav className="flex items-center">
+
+          <details className="md:hidden">
+              <summary className="list-none text-xl">
+                <svg width="38" height="38" aria-hidden="true" viewBox="0 0 24 24">
+                  <path fill="currentColor" d="M3 6h18v2H3zm0 5h18v2H3zm0 5h18v2H3z"></path>
+                </svg>
+                <span className="sr-only">Menu</span>
+              </summary>
+              <nav className="absolute flex flex-col z-10 px-8 py-4 left-0 right-0 top-[86px] w-auto bg-slate-200">
+                <a
+                  href="/case-studies"
+                  className={mobileLinkClasses}
+                >
+                  Case Studios
+                </a>
+                <a
+                  href="/about-us"
+                  className={mobileLinkClasses}
+                >
+                  About Us
+                </a>
+              </nav>
+          </details>
+
+          <nav className="hidden md:flex max-md:mt-6 max-md:py-6 max-md:border-t-2 max-md:border-t-teal-500 items-center">
             <a
-              className="gap-2 hover:underline hover:underline-offset-4"
+              className={twMerge(linkClasses, 'mr-2 md:mr-6')}
               href="/case-studies"
             >
             Case Studios
             </a>
             <a
-              className="gap-2 hover:underline hover:underline-offset-4"
+              className={linkClasses}
               href="/about-us"
             >
               About Us
