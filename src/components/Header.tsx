915a47016754866a403a98985cb96632699731eb
JesseOnslow <40768768+jesseonslow@users.noreply.github.com>
2025-03-21 01:46:41 +0800

    Removed unnecessary tailwind styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,28 +3,24 @@ import Link from "next/link";
 import { Container } from "../components/Container";
 
 export const Header = () => {
-  const linkClasses = "flex uppercase hover:underline hover:underline-offset-4 p-2 md:px-4 md:py-3 ml-2 md:ml-6 text-white bg-blue-800 font-bold hover:bg-blue-600"
+  const linkClasses = "flex uppercase hover:underline hover:underline-offset-4 md:px-4 ml-2 md:ml-6 font-bold"
   const mobileLinkClasses = "px-2 py-4 hover:underline hover:underline-offset-4"
 
   const menuItems = [
-    {
-      label: "Case studies",
-      url: "/case-studies"
-    },
-    {
-      label: "About Us",
-      url: "/about-us"
-    }
+    {label: "Home", url: "/home"},
+    {label: "About", url: "/about"},
+    {label: "Blog", url: "/blog"},
+    {label: "Sign Up", url: "/sign-up"}
   ]
 
   return (
-      <header className="py-4 md:py-8 border-b-2 border-b-slate-200">
+      <header className="pb-1 md:pb-2">
         <Container className="flex justify-between align-middle items-center relative">
-          <Link href="/">
+          <Link href="/home">
             <Image
-              className="dark:invert w-[122px] h-[36px] md:w-[183px] md:h-[54px]"
-              src="/logo-new.svg"
-              alt="Norton Studios Logo"
+              className="dark:invert w-[122px] h-[36px] md:w-[183px] md:h-[54px] rounded-none shadow-none"
+              src="/logo.png"
+              alt="SizeByte Logo"
               width={245}
               height={72}
               priority
@@ -38,7 +34,7 @@ export const Header = () => {
                 </svg>
                 <span className="sr-only">Menu</span>
               </summary>
-              <nav className="absolute flex flex-col z-10 px-8 py-4 left-0 right-0 top-[54px] w-auto bg-slate-200">
+              <nav className="absolute flex flex-col z-10 px-8 py-1 left-0 right-0 top-[54px] w-auto bg-slate-300">
                 {menuItems.map((item) => {
                   return <Link key={item.url} href={item.url} className={mobileLinkClasses}>{item.label}</Link>
                 })}
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -13,12 +13,12 @@ export const Heading = ({ level, children, className }: HeadingProps) => {
   // Dynamically determine the heading tag (h1, h2, etc.)
     const Component = level;
     const classes = {
-        h1: `text-4xl md:text-6xl text-gray-800 font-bold`,
-        h2: `text-3xl md:text-4xl text-gray-800 font-bold`,
-        h3: `text-2xl md:text-3xl text-gray-800 font-bold`,
-        h4: `text-xl md:text-2xl text-gray-800 font-bold`,
-        h5: `text-lg md:text-xl text-gray-800 font-bold`,
-        h6: `text-lg text-gray-800 font-bold`,
+        h1: `text-4xl md:text-6xl`,
+        h2: `text-3xl md:text-4xl`,
+        h3: `text-2xl md:text-3xl`,
+        h4: `text-xl md:text-2xl`,
+        h5: `text-lg md:text-xl`,
+        h6: `text-lg`,
     }[level];
 
   return <Component className={twMerge(classes, className)}>{children}</Component>;
