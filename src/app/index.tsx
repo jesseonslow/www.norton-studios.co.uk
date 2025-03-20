8a6864eaa777b0dc1744d6a18d833690dbc71de1
JesseOnslow <40768768+jesseonslow@users.noreply.github.com>
2025-03-21 01:47:51 +0800

    Created temporary index page

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { Heading } from '../components/Heading';
+
+export default function Home() {
+  return (
+    <div>
+      <Heading level="h1" className="text-center sm:text-left">SizeByte</Heading>
+      <p className="text-lg text-center sm:text-left">Beautiful Websites Made Simple.</p>
+    </div>
+  );
+}
