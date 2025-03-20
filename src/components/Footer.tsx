1e3ec680d50e4bb93dd076d7678040c2c3372872
JesseOnslow <40768768+jesseonslow@users.noreply.github.com>
2025-03-21 01:46:58 +0800

    Updated footer content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,11 @@
-import Image from 'next/image';
 import { Container } from "../components/Container";
 
 export const Footer = () => {
   return (
-    <footer className="py-6 border-t-2 border-t-slate-200">    
+    <footer className="py-1 border-t-2 border-t-slate-200">    
       <Container className="flex justify-between align-middle items-center">
-        <Image src="/small-logo.svg" width={45} height={45} alt="Small Norton Studios Logo" />
-        <p className="text-center md:text-right">&copy; {new Date().getFullYear()} Norton Studios</p>
+        <span className="text-center md:text-left">support@sizebyte.com</span>
+        <span className="text-center md:text-right">&copy; {new Date().getFullYear()} SizeByte</span>
       </Container>
     </footer>
   );
