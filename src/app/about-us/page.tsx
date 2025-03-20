8de3edfcfd2558cd0afcaa86b88775626534cca0
JesseOnslow <40768768+jesseonslow@users.noreply.github.com>
2025-03-21 01:44:20 +0800

    Removed disused files

--- src/app/about-us/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Heading } from '../../components/Heading';
-
-export default function CaseStudies() {
-  return (
-    <div>
-      <Heading level='h1' className="text-center sm:text-left">About Us</Heading>
-    </div>
-  );
-}
